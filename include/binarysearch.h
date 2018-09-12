e21bf5b5a85fa4af52b06a8bb3569c4c67a80f9f
VC Science Labs <Jack_Sherlock1@my.vcccd.edu>
2018-09-12 14:51:04 -0700

    Here's hoping

---
+++ include/binarysearch.h
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using std::cout;
+using std::cout;
+
+namespace edu { namespace vcccd { namespace vc {namespace csv15 {
+	template <class Alpaca>
+	int64_t binarysearch(Alpaca value, Alpaca array[], size_t size) {
+		if (size == 0) return -1;
+		size_t guess = size / 2;
+		if (value == array[guess]) {
+			return guess;
+		}
+
+		if (value < array[guess]) {
+			return binarysearch(value, array, guess);
+		}
+
+		else if (value > array[guess]) {
+			return binarysearch(value, array + guess + 1, guess);
+		}
+		return -1;
+	}
+}}}}
---
+++ src/main.cc
@@ -0,0 +1,11 @@
+#include "binarysearch.h"
+
+using std::cout;
+using std::cout;
+
+int main(int argc, char *argv[]) {
+	int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int64_t index = edu::vcccd::vc::csv15::binarysearch(8, array, 9);
+
+	std::cout << "Index = " << index << std::endl;
+}
