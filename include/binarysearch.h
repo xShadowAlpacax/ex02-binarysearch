bb50362356aeca705825554536ae533c4947e0ef
VC Science Labs <Jack_Sherlock1@my.vcccd.edu>
2018-09-17 14:14:30 -0700

    Updating to work on NULL

--- include/binarysearch.h
+++ include/binarysearch.h
@@ -6,8 +6,12 @@ using std::cout;
 namespace edu { namespace vcccd { namespace vc {namespace csv15 {
 	template <class Alpaca>
 	int64_t binarysearch(Alpaca value, Alpaca array[], size_t size) {
+
+		if (array == NULL) return -1;
 		if (size == 0) return -1;
+
 		size_t guess = size / 2;
+
 		if (value == array[guess]) {
 			return guess;
 		}
